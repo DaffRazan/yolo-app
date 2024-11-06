367c9974a0efa0f42a8d7d855681eac26a73106c
Muhammad Daffa Razan <m.daffa@idgi.co.id>
2024-11-06 16:55:04 +0700

    feat: simulate manual timeout for splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  });
+
+  // SplashScreen.preventAutoHideAsync();
+  // // force splash screen to stay still in 3 secs
+  // setTimeout(SplashScreen.hideAsync, 3000);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
     </Stack>
   );
 }
